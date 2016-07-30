4fa17c1afffb8a796ae453c57eb6c2f740ccfa02
Luis E <cerelac_rox@hotmail.com>
2016-07-30 02:21:00 -0500

    Add python script

---
+++ auth.py
@@ -0,0 +1,5 @@
+consumer_key        = 'owdn2IAwFjaqeiG7XKSxXjWN9'
+consumer_secret     = 'XUmgu3duikxIhIK5BwkCnhbgIUDXdj3cjRWJN2kZN3pRzpZ6Qe'
+access_token        = '725806459984859136-CF1HdjjWcCaEcjpVVJDwmZ0Jhj80rwg'
+access_token_secret = '4Iv94tQzJJnz0ZXmLvvX04LaipbS73eD16OfNaLjPo6k1'
+
